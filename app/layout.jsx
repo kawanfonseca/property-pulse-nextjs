f7c5069bd4983cf156cf7e63124059c244a05b06
Kawan <kawanfonseca@hotmail.com>
2024-09-25 17:56:28 -0300

    latest

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,13 @@
 import "../assets/styles/globals.css";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
-        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
