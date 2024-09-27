5e824f2e4f4ea723156cc509edff13a971a0e77c
Kawan <kawanfonseca@hotmail.com>
2024-09-27 18:02:20 -0300

    Added mongo config

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,15 +1,18 @@
-import Properties from "../../properties.json";
+import connectDB from "@/config/database"
+import Property from "@/models/Property";
 import PropertyCard from "../../components/PropertyCard";
 
-const PropertiesPage = () => {
+const PropertiesPage = async () => {
+  await connectDB();
+  const properties = await Property.find({}).lean();
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        {Properties.length === 0 ? (
+        {properties.length === 0 ? (
           <p>No properties found.</p>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {Properties.map((property) => (
+            {properties.map((property) => (
               <div>
                 <PropertyCard key={property._id} property={property} />
               </div>
