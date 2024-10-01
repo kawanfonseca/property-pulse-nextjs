521339e6594976ad039dfbb52e684747cd80f71c
Kawan <kawanfonseca@hotmail.com>
2024-10-01 18:43:30 -0300

    Starting Server Actions and Creating Properties

--- models/Property.js
+++ models/Property.js
@@ -1,66 +1,91 @@
-import { Schema, model , models} from 'mongoose'
+import { Schema, model, models } from 'mongoose';
 
-const PropertySchema = new Schema({
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const PropertySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: [
+      {
+        type: String,
+      },
+    ],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
   },
-  name: {
-    type: String,
-    required: true
-  },
-  type: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String
-  },
-  location: {
-    street: {
-      street: String,
-      city: String,
-      state: String,
-      zipcode: String
-    }
-  },
-  beds: {
-    type: Number,
-    required: true
-  },
-  baths: {
-    type: Number,
-    required: true
-  },
-  square_feet: {
-    type: Number,
-    required: true
-  },
-  amenities: [{
-    type: String
-  }],
-  rates: {
-    nightly: Number,
-    weekly: Number,
-    monthly: Number
-  },
-  seller_info: {
-    name: String,
-    email: String,
-    phone: String
-  },
-  images: [{
-    type: String
-  }],
-  is_featured: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps: true
-});
+);
 
 const Property = models.Property || model('Property', PropertySchema);
-export default Property
+
+export default Property;
