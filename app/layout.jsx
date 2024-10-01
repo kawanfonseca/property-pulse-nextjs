38c94d1ab4ea48181876efda466693c4bb57f774
Kawan <kawanfonseca@hotmail.com>
2024-10-01 15:31:13 -0300

    Finished Auth Session

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
-import "../public/assets/styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AuthProvider from "../components/AuthProvider";
+import "../public/assets/styles/globals.css";
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,31 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import logo from '../public/assets/images/logo-white.png';
 import profileDefault from '../public/assets/images/profile.png';
 import { FaGoogle } from 'react-icons/fa';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { set } from 'mongoose';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  const profileImage = session?.user?.image
+
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [providers, setProviders] = useState(null);
 
   const pathname = usePathname();
+
+  useEffect(()=>{
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    }
+    setAuthProviders()
+  },[])
 
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
@@ -71,7 +84,7 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                {isLoggedin && (
+                {session && (
                 <Link
                   href='/properties/add'
                   className={ `${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2` }
@@ -84,18 +97,22 @@ const Navbar = () => {
           </div>
 
           {/* <!-- Right Side Menu (Logged Out) --> */}
-          {!isLoggedin && (
+          {!session && (
           <div className='hidden md:block md:ml-6'>
             <div className='flex items-center'>
-              <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                <FaGoogle className='text-white mr-2' />
-                <span>Login or Register</span>
-              </button>
+              {providers && Object.values(providers).map((provider, index) => (
+                <button key={index} 
+                onClick={() => signIn(provider.id)}
+                className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
+                  <FaGoogle className='text-white mr-2' />
+                  <span>Login or Register</span>
+                </button>
+              ))}
             </div>
           </div>
           )}
 
-          {isLoggedin && (
+          {session && (
           <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
             <Link href='/messages' className='relative group'>
               <button
@@ -139,7 +156,9 @@ const Navbar = () => {
                   <span className='sr-only'>Open user menu</span>
                   <Image
                     className='h-8 w-8 rounded-full'
-                    src={profileDefault}
+                    src={profileImage || profileDefault}
+                    width={40}
+                    height={40}
                     alt=''
                   />
                 </button>
@@ -178,6 +197,10 @@ const Navbar = () => {
                     role='menuitem'
                     tabIndex='-1'
                     id='user-menu-item-2'
+                    onClick={() => {
+                      setIsProfileMenuOpen(false)
+                      signOut()
+                    }}
                   >
                     Sign Out
                   </button>
@@ -212,7 +235,7 @@ const Navbar = () => {
             >
               Add Property
             </Link>
-            {!isLoggedIn && (
+            {!session && (
             <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
               <i className='fa-brands fa-google mr-2'></i>
               <span>Login or Register</span>
